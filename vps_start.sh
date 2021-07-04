6b8e4e65f2f62f640651a132a5741c3439a5b85a
dragonstrong <32926995+dragonstrong@users.noreply.github.com>
2021-07-04 12:10:08 +0800

    Update vps_start.sh

--- vps_start.sh
+++ vps_start.sh
@@ -1,4 +1,4 @@
 sudo apt install python3-pip
 pip3 install https://github.com/shadowsocks/shadowsocks/archive/master.zip
-sudo vim /etc/shadowsocks.json
-ssserver -c shadowsocks -d start
+sudo vim shadowsocks.json
+ssserver -c shadowsocks.json -d start
